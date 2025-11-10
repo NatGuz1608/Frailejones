136006bd081877209f79ab6b1d7b555ad185aa57
Natalia Guzman <nataliaguzmanburgos@gmail.com>
2025-11-10 10:00:09 -0500

    Cambios espacio y forma
    Cambios

--- player.gd
+++ player.gd
@@ -3,24 +3,24 @@ extends Area2D
 @export var speed: int = 200
 var velocity: Vector2 = Vector2.ZERO
 var screen_size: Vector2
+var move_area: Rect2 = Rect2(0, 0, 480, 854)
 
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 
 func _process(delta: float) -> void:
 	velocity = Vector2.ZERO
-
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-
 	position += velocity * delta
-	position.x = clamp(position.x, 0.0, screen_size.x)
-	position.y = clamp(position.y, 0.0, screen_size.y)
+	
+	# Limita la posición al área definida
+	position.x = clamp(position.x, move_area.position.x, move_area.end.x)
+	position.y = clamp(position.y, move_area.position.y, move_area.end.y)
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("obstacle"):
